b31346b0bb1a0a84268a7ec25d60816a705e61c0
DEVLSBC <luan.sbc07@gmail.com>
2024-12-05 10:00:01 -0300

    V0.3

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -6,6 +6,35 @@ if (!isset($_SESSION['username'])) {
     exit;
 }
 
-echo "Bem-vindo, " . htmlspecialchars($_SESSION['username']) . "!";
-echo "<br><a href='/hdmjbo/pages/logout.php'>Sair</a>";
+include '../includes/header.php';
+?>
+	<main class="dashboard">
+		<!-- Cabeçalho da página, contendo imagem e título -->
+		<header>
+				<div class="dash_header">
+					<div>
+						<img class="dash_header_logo" src="/hdmjbo/assets/imagens/LOGO.png" alt="Logo da Prefeitura de Fortaleza">
+					</div>
+					<div></div>
+					<div>
+						<h1><?php echo "Bem-vindo(a), " . htmlspecialchars($_SESSION['username']) . "!";?></h1>
+					</div>
+					<div>
+						<a class="dash_sair_button" href='/hdmjbo/pages/logout.php'>Sair</a>						
+					</div>
+				</div>
+
+			<hr>
+		</header>
+		<!-- Rodape da página -->
+		<footer>
+			<hr>
+				<div class="dash_footer">
+					<p>&copy; 2024 HDMJBO. Todos os direitos reservados.</p>
+				</div>
+		</footer>
+	</main>
+
+<?php
+include '../includes/footer.php';
 ?>
